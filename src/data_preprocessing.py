2f88be1c0b80399571c4815371518eedcb6b889e
1cbyc <isaacnsisong@gmail.com>
2024-11-08 09:47:44 +0100

    building a phishing detector cause i can

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -3,4 +3,4 @@ from sklearn.model_selection import train_test_split
 
 def load_data():
     phishing_df = pd.read_csv('data/phishing_urls.csv')
-    legit_df = pd.read_csv
+    legit_df = pd.read_csv('')
