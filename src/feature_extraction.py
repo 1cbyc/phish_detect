c976fd6bd42f2a76cb0d03d9355d46f5622eaa6f
1cbyc <isaacnsisong@gmail.com>
2024-11-08 12:19:11 +0100

    building a phishing detector cause i can

--- src/feature_extraction.py
+++ src/feature_extraction.py
@@ -13,4 +13,6 @@ def extract_metadata_features(urls):
     lengths = urls.apply(lambda x: len(x))
     special_char_count = urls.apply(lambda x: sum(1 for c in x if not c.isalnum()))
 
-    metadata_features
+    metadata_features = pd.DataFrame({
+        
+    })
