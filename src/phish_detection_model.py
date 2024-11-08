38bfd4224c83c38971e727a9420140ea25df2928
1cbyc <isaacnsisong@gmail.com>
2024-11-08 12:49:29 +0100

    building a phishing detector cause i can

---
+++ src/phish_detection_model.py
@@ -0,0 +1,10 @@
+import joblib
+
+def load_model():
+    return joblib.load('src/phish_detection_model.pkl')
+
+def predict_url(url, model, preprocessor):
+    # Transform the URL into features
+    url_features = preprocess_single_url(url)
+    url_transformed = preprocessor.transform([url_features])
+    return model.predict(url_transformed), model.predict_proba(url_transformed)
