e408cd6ae0be58df2175e5c00f8ac880834c3ec3
1cbyc <isaacnsisong@gmail.com>
2024-11-08 09:51:47 +0100

    building a phishing detector cause i can

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -16,4 +16,5 @@ def load_data():
 def split_data(data):
     X = data['url'] # if i run it for the email, i will use 'email' instead of url (depending on the data i use)
     y = data['label']
-    return train_test_split(X, y, test_size=0.2, random)
+    return train_test_split(X, y, test_size=0.2, random_state=42)
+
