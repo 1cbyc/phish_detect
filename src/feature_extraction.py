34900a242b2cbdf3f4b1e0b6c295082c70cbbdb6
1cbyc <isaacnsisong@gmail.com>
2024-11-08 12:23:37 +0100

    building a phishing detector cause i can

--- src/feature_extraction.py
+++ src/feature_extraction.py
@@ -25,4 +25,4 @@ def extract_combined_features(X_train, X_test):
     X_train_text, X_test_text = extract_text_features(X_train, X_test)
 
     # will extract metadata features
-    X_train_meta = 
+    X_train_meta = extract_text_features(X)
