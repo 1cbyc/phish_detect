690c639658930b315cf9526d2fe92091df9193c7
1cbyc <isaacnsisong@gmail.com>
2024-11-08 12:54:08 +0100

    building a phishing detector cause i can

--- web_app/app.py
+++ web_app/app.py
@@ -1,19 +1,24 @@
-from flask import Flask, render_template, request
-from src.phish_detection_model import load_model, predict_url
+# from flask import Flask, render_template, request
+# from src.phish_detection_model import load_model, predict_url
 
-app = Flask(__name__)
-model = load_model()
-preprocessor = None  # to load the preprocessor if saved separately
+# app = Flask(__name__)
+# model = load_model()
+# preprocessor = None  # to load the preprocessor if saved separately
 
-@app.route("/", methods=["GET", "POST"])
-def index():
-    prediction, confidence = None, None
-    if request.method == "POST":
-        url = request.form["url"]
-        prediction, confidence = predict_url(url, model, preprocessor)
-        confidence = round(confidence[0][1] * 100, 2)  # if the 2nd column is phishing probability
+# @app.route("/", methods=["GET", "POST"])
+# def index():
+#     prediction, confidence = None, None
+#     if request.method == "POST":
+#         url = request.form["url"]
+#         prediction, confidence = predict_url(url, model, preprocessor)
+#         confidence = round(confidence[0][1] * 100, 2)  # if the 2nd column is phishing probability
     
-    return render_template("index.html", prediction=prediction, confidence=confidence)
+#     return render_template("index.html", prediction=prediction, confidence=confidence)
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+from web_app.routes import app
+
+if __name__ == '__main__':
     app.run(debug=True)
