8e4d0763d32ef189e1294ecb36d54601ce239d2f
1cbyc <isaacnsisong@gmail.com>
2024-11-08 12:05:02 +0100

    building a phishing detector cause i can

--- src/model_training.py
+++ src/model_training.py
@@ -1,3 +1,3 @@
 import joblib
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, precision_score, 
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
