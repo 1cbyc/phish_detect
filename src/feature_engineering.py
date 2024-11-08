ace01159eccc6730419557d25c0dad80788a414a
1cbyc <isaacnsisong@gmail.com>
2024-11-08 11:51:42 +0100

    building a phishing detector cause i can

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -12,5 +12,5 @@ def preprocess_features(X_train, X_test):
     Preprocessing pipelines for both types of features
         numeric_transformer = Pipeline(steps=[
             ('imputer', SimpleImputer(strategy='mean')),
-            ()
+            ('scaler', StandardScaler())
         ])
