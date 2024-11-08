16afcd0cc02c4b76e24b5dcec65c03bb5be7923e
1cbyc <isaacnsisong@gmail.com>
2024-11-08 09:48:45 +0100

    building a phishing detector cause i can

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -5,4 +5,5 @@ def load_data():
     phishing_df = pd.read_csv('data/phishing_urls.csv')
     legit_df = pd.read_csv('data/legitimate_urls.csv')
 
-    # i want to add a label colum: 1 for p
+    # i want to add a label colum: 1 for phishing and 0 for legit
+    phishing_df['label']
