21508bfe9ebdea97168e50bc9914d4133c9937d2
1cbyc <isaacnsisong@gmail.com>
2024-11-08 11:38:08 +0100

    building a phishing detector cause i can

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -8,10 +8,12 @@ def load_data():
     # legit_df = pd.read_csv('data/legitimate_urls.csv')
 
     # i want to add a label colum: 1 for phishing and 0 for legit
-    phishing_df['label'] = 1
-    legit_df['label'] = 0
+    # phishing_df['label'] = 1
+    # legit_df['label'] = 0
     # since this new dataset has "url" and "label" columns i will just update the column name based on what i read now
     data = data[['url', 'label']]
+
+    # since 
 
     # so now i want to concatenate the datasets and shuffle it
     data = pd.concat([phishing_df, legit_df]).sample(franc=1).reset_index(drop=True)
