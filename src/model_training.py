c9d1a1d65a48dc37be753e6e686e86d50219ce22
1cbyc <isaacnsisong@gmail.com>
2024-11-08 12:08:52 +0100

    building a phishing detector cause i can

--- src/model_training.py
+++ src/model_training.py
@@ -9,4 +9,6 @@ def train_model(X_train, y_train):
     return model
 def evaluate_model(model, X_test, y_test):
     y_pred = model.predict(X_test)
-    
+    return {
+        'accuracy': ac
+    }
