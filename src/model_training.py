9481fb83f6f24587a4870e3dc3c4a1535cb5646f
1cbyc <isaacnsisong@gmail.com>
2024-11-08 13:43:01 +0100

    building a phishing detector cause i can

--- src/model_training.py
+++ src/model_training.py
@@ -1,6 +1,6 @@
 import joblib
 # import pandas as pd
-# from src.feature_extraction import extract_combined_features
+from src.feature_extraction import extract_combined_features
 # from src.model_training import train_model
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
