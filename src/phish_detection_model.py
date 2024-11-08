2dd0d401c487917cd1b68be0a10af1a17e74e413
1cbyc <isaacnsisong@gmail.com>
2024-11-08 12:49:41 +0100

    building a phishing detector cause i can

--- src/phish_detection_model.py
+++ src/phish_detection_model.py
@@ -7,4 +7,4 @@ def predict_url(url, model, preprocessor):
     # Transform the URL into features
     url_features = preprocess_single_url(url)
     url_transformed = preprocessor.transform([url_features])
-    return model.predict(url_transformed), model.predict_proba(url_transformed)
+    return model.predict(url_transformed), model.predict_prob(url_transformed)
