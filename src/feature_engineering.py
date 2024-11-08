b7682a111e5bca4306c040d96d66beb5e62fc214
1cbyc <isaacnsisong@gmail.com>
2024-11-08 11:48:04 +0100

    building a phishing detector cause i can

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -1,4 +1,5 @@
 from sklearn.preprocessing import StandardScaler
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
-from sklearn.pipeline import 
+from sklearn.pipeline import OneHotEncoder
+from sklearn.impute import 
