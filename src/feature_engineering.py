1d7a1e4ccf426f72fae7df296d724634d5dd1ca2
1cbyc <isaacnsisong@gmail.com>
2024-11-08 11:50:29 +0100

    building a phishing detector cause i can

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -7,4 +7,6 @@ from sklearn.impute import SimpleImputer
 def preprocess_features(X_train, X_test):
     # i am trying to define which columns are numeric and which are categorical
     numeric_features = X_train.select_dtypes(include=['int64', 'float64']).columns
-    categorical_features = X_train.select_dtypes(include=['object']).column
+    categorical_features = X_train.select_dtypes(include=['object']).columns
+
+    Preprocessing 
