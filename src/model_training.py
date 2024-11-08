56e0fcf5bc11dac5e1bf2967b2662f5646030aa9
1cbyc <isaacnsisong@gmail.com>
2024-11-08 12:06:39 +0100

    building a phishing detector cause i can

--- src/model_training.py
+++ src/model_training.py
@@ -3,4 +3,5 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
 def train_model(X_train, y_train):
-    model = LogisticRegression(max_iter=1000) # increase 
+    model = LogisticRegression(max_iter=1000) # increase max_iter if necessary
+    model
