9f20976fe47fdda5d48e56a06cc319ce54fb1907
1cbyc <isaacnsisong@gmail.com>
2024-11-08 12:56:21 +0100

    building a phishing detector cause i can

--- web_app/routes.py
+++ web_app/routes.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request
 import joblib
+from src.phish_detection_model import predict_url
 
 app = Flask(__name__)
 model = joblib.load('src/phish_detection_model.pkl')
