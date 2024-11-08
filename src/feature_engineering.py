8e894eaed9d9cebc806ddcd736c34c8b00ada4a5
1cbyc <isaacnsisong@gmail.com>
2024-11-08 11:52:18 +0100

    building a phishing detector cause i can

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -14,4 +14,6 @@ def preprocess_features(X_train, X_test):
             ('imputer', SimpleImputer(strategy='mean')),
             ('scaler', StandardScaler())])
     
-        categorical_tranformer =
+        categorical_tranformer =Pipeline(steps=[
+            
+        ])
