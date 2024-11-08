f90471bc9ba78307dfcfdfc1638391c500164c68
1cbyc <isaacnsisong@gmail.com>
2024-11-08 13:33:43 +0100

    building a phishing detector cause i can

--- src/model_training.py
+++ src/model_training.py
@@ -5,6 +5,8 @@ from src.model_training import train_model
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
+data = pd.read_csv("path_to_dataset.csv")
+
 def train_model(X_train, y_train):
     model = LogisticRegression(max_iter=1000) # increase max_iter if necessary
     model.fit(X_train, y_train)
