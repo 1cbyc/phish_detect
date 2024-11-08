eae6de011e1fa9cec81d88ac28cafeb58c3333ca
1cbyc <isaacnsisong@gmail.com>
2024-11-08 12:09:29 +0100

    building a phishing detector cause i can

--- src/model_training.py
+++ src/model_training.py
@@ -11,5 +11,5 @@ def evaluate_model(model, X_test, y_test):
     y_pred = model.predict(X_test)
     return {
         'accuracy': accuracy_score(y_test, y_pred),
-        'precision'
+        'precision': precision_score(y_test, y_pred)
     }
