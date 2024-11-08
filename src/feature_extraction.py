fec2b096b2a5980c86d462543298fcef31fff362
1cbyc <isaacnsisong@gmail.com>
2024-11-08 12:16:09 +0100

    building a phishing detector cause i can

--- src/feature_extraction.py
+++ src/feature_extraction.py
@@ -5,4 +5,4 @@ import numpy as np
 def extract_text_features(X_train, X_test):
     vectorizer = TfidfVectorizer(max_features=3000, ngram_range=(1, 3), stop_words='english')
     X_train_tfidf = vectorizer.fit_transform(X_train)
-    X_test
+    X_test_t
